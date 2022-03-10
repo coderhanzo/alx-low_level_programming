46b49812c56c44a846675e22bab9f6ef292386a6
coderhanzo <reinerbielert@icloud.com>
2022-03-10 20:55:29 +0000

    adding c file, 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#insert<studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programmingis like  building a multilingual puzzle");
+	return (0);
+}
