481529851b7423ee1ae8e6fefca67c581454065b
coderhanzo <reinerbielert@icloud.com>
2022-03-30 22:06:55 +0000

    made changes to 1, 2, 3, 5, 6 and 100

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
  */
 int sqrt2(int a, int b)
 {
-  if (b * b == a)
-    return (b);
-  else if (b * b > a)
-    return (-1);
-  return (sqrt2(a, b + 1));
+if (b * b == a)
+return (b);
+else if (b * b > a)
+return (-1);
+return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
@@ -25,5 +25,5 @@ int sqrt2(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-  return (sqrt2(n, 1));
+return (sqrt2(n, 1));
 }
